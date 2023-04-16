07435bcb3463eeb7517dd28666b394c233ccf1c4
ebell495 <ericbell495@gmail.com>
2023-04-16 12:49:08 -0700

    feat: update fuzz harness

--- chrs-lib/fuzz/fuzz_targets/chrs-lib-fuzz.rs
+++ chrs-lib/fuzz/fuzz_targets/chrs-lib-fuzz.rs
@@ -1,18 +1,26 @@
 #![no_main]
 
-use libfuzzer_sys::fuzz_target;
-use libfuzzer_sys::arbitrary::{self, Arbitrary};
 use chrs_lib::data::{BoardConfig, BoardPiece, Color, Move, Square};
+use libfuzzer_sys::fuzz_target;
+use std::str::FromStr;
 
-#[derive(Arbitrary, Copy, Clone, Debug)]
-enum StartingText<'a> {
-    Custom(&'a str),
-}
-
-fuzz_target!(|data: StartingText| {
-    let fen = match data {
-        StartingText::Custom(s) => s,
-    };
-    
-    let mut config = BoardConfig::from_fen_str(&fen);
+fuzz_target!(|data: Vec<&str>| {
+    let mut config = BoardConfig::default();
+    for m in data {
+        if m.len() >= 4 {
+            let chars = m.chars();
+            match Square::from_str(&chars.clone().take(2).collect::<String>()) {
+                Ok(from) => {
+                    match Square::from_str(&chars.clone().skip(2).take(2).collect::<String>()) {
+                        Ok(to) => {
+                            let mut m = Move::infer(from, to, &config);
+                        },
+                        Err(_) => ()
+                    }
+                },
+                Err(_) => ()
+            }
+            
+        }
+    }
 });
